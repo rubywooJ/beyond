db63c9eb3d261ca429feeaba085f55e48497f8f9
ruby woo <i520gfy@gmail.com>
2020-03-19 17:29:41 +0800

    removed class JwtUtil

--- src/main/java/cn/tsxygfy/beyond/configuration/WebMvcAutoConfig.java
+++ src/main/java/cn/tsxygfy/beyond/configuration/WebMvcAutoConfig.java
@@ -1,9 +1,6 @@
 package cn.tsxygfy.beyond.configuration;
 
-import cn.tsxygfy.beyond.interceptor.JwtInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,9 +17,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcAutoConfig implements WebMvcConfigurer {
 
-    @Autowired
-    JwtInterceptor jwtInterceptor;
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
@@ -30,8 +24,4 @@ public class WebMvcAutoConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/admin/**").addResourceLocations("classpath:/admin/");
     }
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        //registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/**").excludePathPatterns("/api/admin/login");
-    }
 }
